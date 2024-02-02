3b7ba351f6537d66a8f5f45030884cf2a44d92a2
Zachary Everton <evertonzachary@gmail.com>
2024-02-02 13:04:36 -0700

    Final backup (for real :)

--- convex_hull.py
+++ convex_hull.py
@@ -69,34 +69,15 @@ class ConvexHullSolver(QObject):
         points.sort(key=lambda p: p.x())
         t2 = time.time()
         t3 = time.time()
+        #Calling the hull function on the sorted points
         polygon = topBottomSplit(points)
-        # TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
         t4 = time.time()
 
-        # when passing lines to the display, pass a list of QLineF objects.  Each QLineF
-        # object can be created with two QPointF objects corresponding to the endpoints
         self.showHull(polygon,RED)
         self.showText('Time Elapsed (Convex Hull): {:3.5f} sec'.format(t4-t3))
 
 
-def QPointXSort(points):
-    if len(points) == 1:
-        return points
-    else:
-        pointsFinal = []
-        points1 = QPointXSort(points[:len(points)//2])
-        points2 = QPointXSort(points[len(points)//2:])
-        while (len(points1) > 0) & (len(points2) > 0):
-            if points1[0].x() > points2[0].x():
-                pointsFinal.append(points2[0])
-                points2 = points2[1:]
-            else:
-                pointsFinal.append(points1[0])
-                points1 = points1[1:]
-        
-        pointsFinal = pointsFinal+points1+points2
-        return pointsFinal
-
+#This function splits the sorted points into an upper and lower part, then calls the convex hull functions on each part
 def topBottomSplit(points):
     slope = (points[-1].y() - points[0].y()) / (points[-1].x() - points[0].x())
     topPoints = [points[0]]
@@ -106,52 +87,64 @@ def topBottomSplit(points):
             topPoints.append(p)
         else:
             bottomPoints.append(p)
+    #After this last append, the top and bottom point arrays each start with the farthest left point overall, and end with the farthest right point overall.
+    #These points are guaranteed to be in the hull so it is good to use them for joining later on.
     topPoints.append(points[-1])
     bottomPoints.append(points[-1])
 
+    #Calling the actual hull functions on each half.
     topHull = QPointTopHull(topPoints)
     bottomHull = QPointBottomHull(bottomPoints)
+    #Combining the halfs (bottom half gets reversed so the whole thing is in clockwise order, and duplicate ends are taken off.
     orderedPoints = topHull[:-1]+bottomHull[-1:0:-1]
+    #This final line turns the array of points into an array of lines
     return [QLineF(orderedPoints[i], orderedPoints[(i + 1) % len(orderedPoints)]) for i in range(len(orderedPoints))]
 
 def QPointTopHull(points):
+    #Normally, we would have a set of points size 3 as our base case. Since I split the top and bottom parts,
+    #I'm not sure if the point in the middle of a set of 3 should be in the hull. If the first and last point are in the final hull, the middle point will not be checked.
+    #So, we set the base case as 1 or 2 points, not 3.
     if len(points) <= 2:
         return points
     else:
+        #Splitting into left and right halves to call recursively
         leftHull = QPointTopHull(points[:len(points)//2])
         rightHull = QPointTopHull(points[len(points)//2:])
-        #find upper connection first:
+        #These indexes represent the farthest right point on the left part and the farthest left point on the right part
         leftEndIndex = len(leftHull) - 1
         rightEndIndex = 0
         modified = True
+        #loop until we do a full iteration without changing left or right end index
         while modified:
             modified = False
-            while isBelow(leftHull[leftEndIndex], rightHull[rightEndIndex], leftHull[:leftEndIndex]): #(getAngle(leftHull[leftEndIndex-1], leftHull[leftEndIndex], rightHull[rightEndIndex]) < 180) & (leftEndIndex > 0):
+            while isBelow(leftHull[leftEndIndex], rightHull[rightEndIndex], leftHull[leftEndIndex-1:leftEndIndex]):
+                #if the upper tangent line is above the next point (if it exists)
                 leftEndIndex = (leftEndIndex - 1)
                 modified = True
-            while isBelow(leftHull[leftEndIndex], rightHull[rightEndIndex], rightHull[rightEndIndex+1:]): #(getAngle(leftHull[leftEndIndex], rightHull[rightEndIndex], rightHull[rightEndIndex+1-len(rightHull)]) < 180) & (rightEndIndex < len(rightHull)):
+            while isBelow(leftHull[leftEndIndex], rightHull[rightEndIndex], rightHull[rightEndIndex+1:rightEndIndex+2]):
+                #if the upper tangent line is below the next point (if it exists)
                 rightEndIndex = (rightEndIndex + 1)
                 modified = True
 
         finalPoints = leftHull[:leftEndIndex+1]+rightHull[rightEndIndex:]
         return finalPoints
 
+#This function is basically the same as the top function, just checking for lower tangent when combining instead of upper tangent
 def QPointBottomHull(points):
     if len(points) <= 2:
         return points
     else:
         leftHull = QPointBottomHull(points[:len(points)//2])
         rightHull = QPointBottomHull(points[len(points)//2:])
-        #find upper connection first:
         leftEndIndex = len(leftHull) - 1
         rightEndIndex = 0
         modified = True
         while modified:
             modified = False
-            while isAbove(leftHull[leftEndIndex], rightHull[rightEndIndex], leftHull[:leftEndIndex]):
+            while isAbove(leftHull[leftEndIndex], rightHull[rightEndIndex], leftHull[leftEndIndex-1:leftEndIndex]):
                 leftEndIndex = (leftEndIndex - 1)
                 modified = True
-            while isAbove(leftHull[leftEndIndex], rightHull[rightEndIndex], rightHull[rightEndIndex+1:]):
+            while isAbove(leftHull[leftEndIndex], rightHull[rightEndIndex], rightHull[rightEndIndex+1:rightEndIndex+2]):
                 rightEndIndex = (rightEndIndex + 1)
                 modified = True
 
@@ -159,7 +152,7 @@ def QPointBottomHull(points):
         return finalPoints
 
 
-
+#Functions for examining if any of the points are above or below a specific line.
 def isBelow(leftEnd, rightEnd, testpoints):
     slope = (rightEnd.y() - leftEnd.y()) / (rightEnd.x() - leftEnd.x())
     for p in testpoints:
